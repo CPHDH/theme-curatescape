75d68df117f496d2d3e9c4af90f8e78c9d8482d1
Erin Bell <ebellempire@gmail.com>
2013-11-14 14:11:11 -0500

    path update

--- si_gardens/geolocation/map/browse.php
+++ si_gardens/geolocation/map/browse.php
@@ -1,4 +1,4 @@
 <?php 
 // We don't need to use the Geolocation plugin on the front-end
-include_once($_SERVER["DOCUMENT_ROOT"] . "/themes/curatescape/error/404.php");
+include_once($_SERVER["DOCUMENT_ROOT"] . "/themes/si_gardens/error/404.php");
 ?>
