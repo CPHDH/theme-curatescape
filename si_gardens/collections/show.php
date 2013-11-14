40055650cdf3633f3cf22a272f5d723c4f551850
Erin Bell <ebellempire@gmail.com>
2013-11-14 13:35:21 -0500

    collection-->error templates: update theme path

--- si_gardens/collections/show.php
+++ si_gardens/collections/show.php
@@ -1,4 +1,4 @@
 <?php 
 // We don't use the Collections on the front-end
-include_once($_SERVER["DOCUMENT_ROOT"] . '/themes/curatescape/error/404.php');
+include_once($_SERVER["DOCUMENT_ROOT"] . '/themes/si_gardens/error/404.php');
 ?>
