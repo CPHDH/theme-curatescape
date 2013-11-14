a2a59e1075c537394512f21debdaf20a748b4dd8
Erin Bell <ebellempire@gmail.com>
2013-11-14 13:26:28 -0500

    Contribution form template customizations

--- si_gardens/contribution/contribution/contribute.php
+++ si_gardens/contribution/contribution/contribute.php
@@ -6,7 +6,7 @@
  * @package Contribution
  */
 
-$head = array('title' => 'Contribute',
+$head = array('title' => 'Share A Story',
               'bodyclass' => 'contribution');
 head($head); ?>
 <?php echo js('contribution-public-form'); ?>
@@ -14,6 +14,17 @@ head($head); ?>
 // <![CDATA[
 enableContributionAjaxForm(<?php echo js_escape(uri('contribution/type-form')); ?>);
 // ]]>
+</script>
+
+<script type="text/javascript">
+	//Reveal more file uploads
+	jQuery(document).ready(function() {
+		jQuery('.upload-more a').click(function(event){
+			event.preventDefault();
+			jQuery('.contribution article form .upload-hidden').slideDown();
+			jQuery(this).hide();
+		});
+	});
 </script>
 
 <div id="content">
@@ -28,23 +39,14 @@ enableContributionAjaxForm(<?php echo js_escape(uri('contribution/type-form')); 
 
 
 	<div id="primary" class="show" role="main">
-<?php echo flash(); ?>
+	<?php echo flash(); ?>
     
     <form method="post" action="" enctype="multipart/form-data">
-        <fieldset id="contribution-item-metadata">
-            <div class="inputs">
-                <label for="contribution-type">What type of item do you want to contribute?</label>
-                <?php echo contribution_select_type(array( 'name' => 'contribution_type', 'id' => 'contribution-type'), $_POST['contribution_type']); ?>
-                <input type="submit" name="submit-type" id="submit-type" value="Select" />
-            </div>
-            <div id="contribution-type-form">
-            <?php if (isset($typeForm)): echo $typeForm; endif; ?>
-            </div>
-        </fieldset>
+    
         <fieldset id="contribution-contributor-metadata" <?php if (!isset($typeForm)) { echo 'style="display: none;"'; }?>>
-            <legend>Personal Information</legend>
+            <h3>About You</h3>
             <div class="field">
-                <label for="contributor-name">Name</label>
+                <label for="contributor-name">Name <span>Only your first name and last initial will be published on the web.</span></label>
                 <div class="inputs">
                     <div class="input">
                         <?php echo $this->formText('contributor-name', $_POST['contributor-name'], array('class' => 'textinput')); ?>
@@ -52,7 +54,7 @@ enableContributionAjaxForm(<?php echo js_escape(uri('contribution/type-form')); 
                 </div>
             </div>
             <div class="field">
-                <label for="contributor-email">Email Address</label>
+                <label for="contributor-email">Email Address <span>We will never share your email address with any third party or on the web. We will only contact you if we need clarification about your entry.</span></label>
                 <div class="inputs">
                     <div class="input">
                         <?php echo $this->formText('contributor-email', $_POST['contributor-email'], array('class' => 'textinput')); ?>
@@ -64,19 +66,32 @@ enableContributionAjaxForm(<?php echo js_escape(uri('contribution/type-form')); 
             echo $field;
         }
         ?>
-        </fieldset>
+        </fieldset>    
+    
+    
+        <div class="inputs hidden">
+            <label for="contribution-type">What type of item do you want to contribute?</label>
+            <?php echo contribution_select_type(array( 'name' => 'contribution_type', 'id' => 'contribution-type'), $_POST['contribution_type']); ?>
+            <input type="submit" name="submit-type" id="submit-type" value="Select" />
+        </div>
+        <div id="contribution-type-form">
+        <?php if (isset($typeForm)): echo $typeForm; endif; ?>
+        </div>
+
+
         <fieldset id="contribution-confirm-submit" <?php if (!isset($typeForm)) { echo 'style="display: none;"'; }?>>
             <div id="captcha" class="inputs"><?php echo $captchaScript; ?></div>
-            <div class="inputs">
-                <?php echo $this->formCheckbox('contribution-public', $_POST['contribution-public'], null, array('1', '0')); ?>
+            <div class="inputs hidden">
+                <?php echo $this->formCheckbox('contribution-public', '1', null, array('1','0')); ?>
                 <?php echo $this->formLabel('contribution-public', 'Publish my contribution on the web.'); ?>
             </div>
-            <p>In order to contribute, you must read and agree to the <a href="<?php echo uri('contribution/terms') ?>" target="_blank">Terms and Conditions.</a></p>
+            <p>I am over the age of 13 and have read and agree to the <a href="<?php echo uri('contribution/terms') ?>" target="_blank">Terms and Conditions</a>. <span>Or, I am an educator submitting work produced by my students and I have read and agree to all the Terms & Conditions and have collected the <a href="<?php echo uri('#') ?>" target="_blank">Parental Permission Slips</a> for my own records that allow my students to participate in this project.</span></p>
             <div class="inputs">
                 <?php echo $this->formCheckbox('terms-agree', $_POST['terms-agree'], null, array('1', '0')); ?>
-                <?php echo $this->formLabel('terms-agree', 'I agree to the Terms and Conditions.'); ?>
+                <?php echo html_entity_decode($this->formLabel('terms-agree', 'I agree to the <a href="'.uri('contribution/terms').'" target="_blank">Terms and Conditions</a> and, if applicable, have collected <a href="'.uri('#').'" target="_blank">Parental Permission Slips</a>.')); ?>
             </div>
             <?php echo $this->formSubmit('form-submit', 'Contribute', array('class' => 'submitinput')); ?>
+            <span id="help">Questions or problems? Contact us at <a href="mailto:communityofgardens">communityofgardens@si.edu</a>.</span>
         </fieldset>
     </form>
 
--- si_gardens/contribution/contribution/type-form.php
+++ si_gardens/contribution/contribution/type-form.php
@@ -10,19 +10,79 @@ if ($type->isFileRequired()):
         <?php echo $this->formFile('contributed_file', array('class' => 'fileinput')); ?>
 </div>
 <?php 
-endif;
+endif;?>
+<fieldset>
+<h3>Your Story</h3>
+<?php
 foreach ($type->getTypeElements() as $element) {
-    echo $this->elementForm($element, $item);
-}
+    echo html_entity_decode($this->elementForm($element, $item));
+}?>
+</fieldset>
+<?php
 if (!isset($required) && $type->isFileAllowed()):
 ?>
+<!--div class="field">
+        <?php //echo $this->formLabel('contributed_file', 'Upload a file (Optional)'); ?>
+        <?php //echo $this->formFile('contributed_file', array('class' => 'fileinput')); ?>
+</div-->
+<!-- Adding a altenate/additional file input form requires adding the field name (eg contributed_file_02) to the array in plugins/Contribution/controllers/ContributionController.php on line 204 -->
+<fieldset>
+<h3>Upload Files</h3>
+<span id="upload">Please upload any files that illustrate your story. You can upload up to ten photo, video, or audio files.</span>
 <div class="field">
-        <?php echo $this->formLabel('contributed_file', 'Upload a file (Optional)'); ?>
-        <?php echo $this->formFile('contributed_file', array('class' => 'fileinput')); ?>
+        <?php echo $this->formLabel('contributed_file_01', 'Upload a file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_01', array('class' => 'fileinput')); ?>
 </div>
+<div class="upload-hidden">
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_02', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_02', array('class' => 'fileinput')); ?>
+</div>
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_03', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_03', array('class' => 'fileinput')); ?>
+</div>
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_04', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_04', array('class' => 'fileinput')); ?>
+</div>
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_05', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_05', array('class' => 'fileinput')); ?>
+</div>
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_06', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_06', array('class' => 'fileinput')); ?>
+</div>
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_07', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_07', array('class' => 'fileinput')); ?>
+</div>
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_08', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_08', array('class' => 'fileinput')); ?>
+</div>
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_09', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_09', array('class' => 'fileinput')); ?>
+</div>
+<div class="field">
+        <?php echo $this->formLabel('contributed_file_10', 'Upload another file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file_10', array('class' => 'fileinput')); ?>
+</div>
+</div>
+<div class="upload-more"><a href="">Upload Additional Files +</a></div>
+</fieldset>
 <?php 
 endif;
+?>
+<fieldset>
+<h3>Garden Location</h3>
+<span id="location">If your story is about a publicly-accessible garden, please enter the full street address. If your story is about a private garden, please only enter the neighborhood, city, and state. For example, “Woodley Park, Washington, D.C.” We will never reveal the actual address of a private garden. The pin on the map will only show the general location of a private garden to protect personally identifiable information.</span>
+<?php
 // Allow other plugins to append to the form (pass the type to allow decisions
 // on a type-by-type basis).
 fire_plugin_hook('contribution_append_to_type_form', $type);
 endif;
+?>
+</fieldset>
