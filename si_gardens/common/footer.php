c66e174561237297b22429ce618f4ab30d1063c0
Erin Bell <ebellempire@gmail.com>
2013-11-15 14:19:46 -0500

    footer: social media and legal info

--- si_gardens/common/footer.php
+++ si_gardens/common/footer.php
@@ -9,13 +9,21 @@
 	    </div>  
 	    
 	    <?php echo random_item_link();?>	
-	        
-    </nav>	
+	    
+        <div class="social-media">
+    		<?php echo social_media(); ?>
+    	</div>
+    
+    	<ul class="navigation navigation-legal">
+	    	<?php echo mh_legal_nav(); ?>      	
+		</ul>
+        
+    </nav>
  
 	<p class="default">
 		Powered by <a href="http://omeka.org/">Omeka</a> + <a href="http://curatescape.org">Curatescape</a>
 		<br>
-		<span id="copyright">&copy; <?php echo date('Y').' '.mh_owner_link();?></span> 
+		<span id="copyright">Copyright &copy; <?php echo date('Y').' '.mh_owner_link();?></span> 
 		<br>
 		<span id="app-store-links"><?php mh_appstore_footer(); ?></span>
 	</p>
