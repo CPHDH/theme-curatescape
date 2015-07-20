4f8a3dca865413f3fcd83d45721a923eb4bac3fb
Erin Bell <euclid@Erin-Bells-MacBook-Pro.local>
2015-07-20 14:46:14 -0400

    exhibit templates

--- si_gardens/exhibit-builder/exhibits/show.php
+++ si_gardens/exhibit-builder/exhibits/show.php
@@ -2,24 +2,25 @@
 
 <div id="content">
 <article class="page show">
-
-	<div id="page-col-left">
-		<aside>
+	
+	<h1><?php echo metadata('exhibit', 'title'); ?></h1>
+	
+	<div id="secondary">
+		<aside class="navigation">
 		<!-- add left sidebar content here -->
+			<h3><?php echo __('Sections'); ?></h3>
+			<?php echo exhibit_builder_page_nav(); ?>
 		</aside>
+		
+		<aside>
+			<a href="<?php echo absolute_url('exhibits'); ?>">More online exhibits</a>
+		</aside>
+		
 	</div>
 
-
 	<div id="primary" class="show" role="main">
-		<h2 class="instapaper_title"><?php echo metadata('exhibit_page', 'title'); ?></h2>
-		<nav id="exhibit-pages">
-		    <?php echo exhibit_builder_page_nav(); ?>
-		</nav>		
-		
-		<nav id="exhibit-child-pages">
-		    <?php echo exhibit_builder_child_page_nav(); ?>
-		</nav>
-		
+		<h2><?php echo metadata('exhibit_page', 'title'); ?></h2>	
+
 		<?php exhibit_builder_render_exhibit_page(); ?>
 		
 		<div id="exhibit-page-navigation">
@@ -33,21 +34,9 @@
 		    <?php echo $nextLink; ?>
 		    </div>
 		    <?php endif; ?>
-		    <div id="exhibit-nav-up">
-		    <?php echo exhibit_builder_page_trail(); ?>
-		    </div>
 		</div>
+		
 	</div>
-
-
-	<div id="page-col-right">
-		<aside id="page-sidebar">
-
-			<!-- Grab some recent images for the image tile montage -->
-			<?php mh_display_recent_item(10);?>
-			
-		</aside>	
-	</div>	
 
 <div id="share-this" class="show">
 <?php echo mh_share_this('Exhibit');?>
