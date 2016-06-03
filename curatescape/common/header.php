d6d8256b423ee62ef0a73ec72f58ac15002b4393
Erin Bell <rhodes@rt-12-01137-148-120-142.dhcp.csuohio.edu>
2016-06-03 09:22:07 -0400

    Fixes mixed content error on HTTPS
    Ref: https://github.com/CPHDH/Curatescape/issues/47

--- curatescape/common/header.php
+++ curatescape/common/header.php
@@ -67,8 +67,8 @@ if ($uploaded_stylesheet=get_theme_option('custom stylesheet')){
 
 <!-- JavaScripts -->
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
-<script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
+<link rel="stylesheet" href="//cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
+<script src="//cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
 <?php if(get_theme_option('clustering')):?>
 	<script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/leaflet.markercluster.js'></script>
 	<link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.css' rel='stylesheet' />
