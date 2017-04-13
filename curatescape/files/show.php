d85866940e5b4585932d19c4e4c316ddfe7f5e43
Erin Bell <ebellempire@gmail.com>
2017-04-13 17:22:07 -0400

    improves and simplifies files/show template

--- curatescape/files/show.php
+++ curatescape/files/show.php
@@ -27,7 +27,7 @@
 		<?php 
 		$record=get_record_by_id('Item', $file->item_id);
 		$title=metadata($record,array('Dublin Core','Title'));
-		echo link_to_item('<i class="icon-chevron-left"></i> '.__('This file appears in: <em><strong>%s</strong></em>', $title), array('class'=>'file-appears-in-item'), 'show', $record);	
+		echo link_to_item(__('This file appears in: <em><strong>%s</strong></em>', $title), array('class'=>'file-appears-in-item'), 'show', $record);	
 		?> 
 		<hr>
 		
@@ -42,46 +42,11 @@
 		//echo link_to_file_edit($file);
 		?>	
 		</div>	
-		
-		<hr>
-		<?php echo link_to_item('<i class="icon-chevron-left"></i> '.__('This file appears in: <em><strong>%s</strong></em>', $title), array('class'=>'file-appears-in-item'), 'show', $record);?> 
-		<hr>
-		
-		<!--a class="big-button reveal-button">View Full Metadata Record</a-->
-		
-		<div id="file-metadata">
-			<?php echo all_element_texts('file'); ?> 		
-	
-		    <div id="format-metadata" class="element-set">
-		        <h2><?php echo __('Format Metadata'); ?></h2>
-		        <div id="original-filename" class="element">
-		            <h3><?php echo __('Original Filename'); ?></h3>
-		            <div class="element-text"><?php echo metadata('file', 'Original Filename'); ?></div>
-		        </div>
-		    
-		        <div id="file-size" class="element">
-		            <h3><?php echo __('File Size'); ?></h3>
-		            <div class="element-text"><?php echo __('%s bytes', metadata('file', 'Size')); ?></div>
-		        </div>
-		
-		    </div><!-- end format-metadata -->
-		    
-		    <div id="type-metadata" class="section element-set">
-		        <h2><?php echo __('Type Metadata'); ?></h2>
-		        <div id="mime-type-browser" class="element">
-		            <h3><?php echo __('Mime Type'); ?></h3>
-		            <div class="element-text"><?php echo metadata('file', 'MIME Type'); ?></div>
-		        </div>
-		        <div id="file-type-os" class="element">
-		            <h3><?php echo __('File Type / OS'); ?></h3>
-		            <div class="element-text"><?php echo metadata('file', 'Type OS'); ?></div>
-		        </div>
-		    </div><!-- end type-metadata -->
-		    
-		</div><!-- end file-metadata -->
+
 	<hr>	
-	<?php echo link_to_item('<i class="icon-chevron-left"></i> '.__('This file appears in: <em><strong>%s</strong></em>', $title), array('class'=>'file-appears-in-item'), 'show', $record);?> 
+	<?php echo link_to_item(__('This file appears in: <em><strong>%s</strong></em>', $title), array('class'=>'file-appears-in-item'), 'show', $record);?> 
 	<hr>
+	<?php echo mh_homepage_hero_item($record);?>
 	
 	</div><!-- end primary -->
 	
